598f9534ceafcc18009632f4ddd30d990250b75b
Angelo Dias <oiangelodias@gmail.com>
2021-03-03 20:13:10 -0300

    Fix kids folders

--- src/get_file_group.py
+++ src/get_file_group.py
@@ -2,6 +2,7 @@ import os
 import random
 from flash_path import flash_path
 
+flash = flash_path()
 
 def find_texts(string):
     return string.endswith(".txt")
@@ -10,17 +11,17 @@ def find_texts(string):
 def get_file_group(chosen, choice):
     dir_list = os.listdir(chosen)  # returns list
 
-    if choice != "comics":
+    if choice not in ["comics", "kids"]:
         return [file for file in filter(find_texts, dir_list)]
 
     selected_image_folder = random.choice(dir_list)
-    selected_image_path = str(os.path.join(flash_path, "comics", selected_image_folder))
+    selected_image_path = str(os.path.join(flash, choice, selected_image_folder))
     dir_list = os.listdir(selected_image_path)
     image_group = []
     for image in dir_list:
         if image.endswith(".jpg"):
             image_path = str(
-                os.path.join(flash_path, "comics", selected_image_folder, image)
+                os.path.join(flash, choice, selected_image_folder, image)
             )
             image_group.append(image_path)
     return image_group
